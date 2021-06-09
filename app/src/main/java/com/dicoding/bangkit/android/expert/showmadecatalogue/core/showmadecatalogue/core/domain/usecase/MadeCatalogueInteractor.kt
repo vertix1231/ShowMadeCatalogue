0dc3d816f96eeede9a084b1d3b4f6a5705e674eb
Irsyad Muhammad Andre <irsyad_vertix@apps.ipb.ac.id>
2021-06-09 11:22:58 +0700

    made dicoding bangkit 2021 expert submisi 2-1 test

--- app/src/main/java/com/dicoding/bangkit/android/expert/showmadecatalogue/core/showmadecatalogue/core/domain/usecase/MadeCatalogueInteractor.kt
+++ app/src/main/java/com/dicoding/bangkit/android/expert/showmadecatalogue/core/showmadecatalogue/core/domain/usecase/MadeCatalogueInteractor.kt
@@ -1,16 +1,16 @@
 package com.dicoding.bangkit.android.expert.showmadecatalogue.core.showmadecatalogue.core.domain.usecase
 
-import androidx.lifecycle.LiveData
 import com.dicoding.bangkit.android.expert.showmadecatalogue.core.showmadecatalogue.core.data.MadeCatalogueRepository
 import com.dicoding.bangkit.android.expert.showmadecatalogue.core.showmadecatalogue.core.data.Resource
 import com.dicoding.bangkit.android.expert.showmadecatalogue.core.showmadecatalogue.core.domain.model.MadeCatalogue
+import kotlinx.coroutines.flow.Flow
 
 class MadeCatalogueInteractor(private val madeCatalogueRepository: MadeCatalogueRepository):MadeCatalogueUseCase {
-    override fun getAllMadeCatalogue(): LiveData<Resource<List<MadeCatalogue>>> {
+    override fun getAllMadeCatalogue(): Flow<Resource<List<MadeCatalogue>>> {
        return madeCatalogueRepository.getAllMadeCatalogue()
     }
 
-    override fun getFavoriteMadeCatalogue(): LiveData<List<MadeCatalogue>> {
+    override fun getFavoriteMadeCatalogue(): Flow<List<MadeCatalogue>> {
         return madeCatalogueRepository.getFavoriteMadeCatalogue()
     }
 
